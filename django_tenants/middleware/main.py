38bb73a1e0734e0430880dfaa0ca5e5620c714c0
ruchitmicro <ruchitkharwa@gmail.com>
2024-02-13 21:22:39 +0530

    sync push

--- django_tenants/middleware/main.py
+++ django_tenants/middleware/main.py
@@ -8,6 +8,7 @@ from django.utils.deprecation import MiddlewareMixin
 from django_tenants.utils import remove_www, get_public_schema_name, get_tenant_types, \
     has_multi_type_tenants, get_tenant_domain_model, get_public_schema_urlconf
 
+from public.models import Tenant
 
 class TenantMainMiddleware(MiddlewareMixin):
     TENANT_NOT_FOUND_EXCEPTION = Http404
@@ -83,3 +84,84 @@ class TenantMainMiddleware(MiddlewareMixin):
             if (hasattr(settings, 'PUBLIC_SCHEMA_URLCONF') and
                     (force_public or request.tenant.schema_name == get_public_schema_name())):
                 request.urlconf = settings.PUBLIC_SCHEMA_URLCONF
+
+
+class TenantUrlMainMiddleware(MiddlewareMixin):
+    TENANT_NOT_FOUND_EXCEPTION = Http404
+
+    def tenant_id_from_request_path(self, request):
+        """
+        Extracts tenant ID from the request path.
+        Assumes the format /<tenant_id>/rest/of/path and strips leading slashes.
+        """
+        try:
+            path_chunks = request.path_info.strip("/").split("/")
+            tenant_id = path_chunks[0]  # Assumes tenant ID is the first part of the path
+            return tenant_id
+        except IndexError:
+            return None  # or raise an appropriate exception
+
+    def get_tenant(self, domain_model, tenant_id):
+        """
+        Overridden to fetch tenant using the tenant_id from the path.
+        """
+        domain = domain_model.objects.select_related('tenant').get(tenant__id=tenant_id)
+        return domain.tenant
+
+    def process_request(self, request):
+        connection.set_schema_to_public()
+        try:
+            tenant_id = self.tenant_id_from_request_path(request)
+            if not tenant_id:
+                raise ValueError("Tenant ID not found in URL path.")
+        except ValueError as e:
+            return self.no_tenant_found(request, tenant_id)
+
+        domain_model = get_tenant_domain_model()
+        try:
+            # tenant = self.get_tenant(domain_model, tenant_id)
+            tenant = Tenant.objects.get(id=tenant_id)
+        except Exception as e:
+            self.no_tenant_found(request, tenant_id)
+            return
+
+        tenant.domain_url = request.get_host()  # Keeps the original host for any further use
+        request.tenant = tenant
+        connection.set_tenant(request.tenant)
+        self.setup_url_routing(request)
+
+
+    def no_tenant_found(self, request, hostname):
+        """ What should happen if no tenant is found.
+        This makes it easier if you want to override the default behavior """
+        if hasattr(settings, 'SHOW_PUBLIC_IF_NO_TENANT_FOUND') and settings.SHOW_PUBLIC_IF_NO_TENANT_FOUND:
+            self.setup_url_routing(request=request, force_public=True)
+        else:
+            raise self.TENANT_NOT_FOUND_EXCEPTION('No tenant for hostname "%s"' % hostname)
+
+    @staticmethod
+    def setup_url_routing(request, force_public=False):
+        """
+        Sets the correct url conf based on the tenant
+        :param request:
+        :param force_public
+        """
+        public_schema_name = get_public_schema_name()
+        if has_multi_type_tenants():
+            tenant_types = get_tenant_types()
+            if (not hasattr(request, 'tenant') or
+                    ((force_public or request.tenant.schema_name == get_public_schema_name()) and
+                     'URLCONF' in tenant_types[public_schema_name])):
+                request.urlconf = get_public_schema_urlconf()
+            else:
+                tenant_type = request.tenant.get_tenant_type()
+                request.urlconf = tenant_types[tenant_type]['URLCONF']
+            set_urlconf(request.urlconf)
+
+        else:
+            # Do we have a public-specific urlconf?
+            if (hasattr(settings, 'PUBLIC_SCHEMA_URLCONF') and
+                    (force_public or request.tenant.schema_name == get_public_schema_name())):
+                request.urlconf = settings.PUBLIC_SCHEMA_URLCONF
+
+
